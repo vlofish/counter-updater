94468bd3ca27bb72a44e6baebccfdef86e761a82
Victor Lopez (UST <U34006@ust-global.com>
2018-05-04 08:53:31 -0500

    $firebaseObject moved here. updateSheltersAvailable function added.

--- src/js/svc/webServicesSvc.js
+++ src/js/svc/webServicesSvc.js
@@ -1,9 +1,31 @@
 (function () {
-
-  'use strict';
+    'use strict';
   
-  angular.module('counterUpdater').service('webServicesSvc', ['$http', 'constants', function ($http, constants) {
+    angular.module('counterUpdater').service('webServicesSvc', ['$http', '$firebaseObject', 'constants', function ($http, $firebaseObject, constants) {
+    
+        /**
+	     * Firebase needed configuration
+	     */
+        $firebaseObject(firebase.database().ref().child(constants.REFERENCE00));
+        var DB = firebase.database();
+     
+        var getEventList = function() {
+            return DB.ref(constants.REFERENCE01).once(constants.VAL).then(function(snapshot) {
+                return snapshot.val();
+            });
+        }
 
+        var updateSheltersAvailable = function(referencePath, eventSelectedObj) {       
+            return DB.ref(referencePath).set(eventSelectedObj).then(function() {
+            }, function(error) {
+                return error;
+            });
+        }
+
+        return {
+            getEventList            : getEventList,
+            updateSheltersAvailable : updateSheltersAvailable
+        }
       /**
        * WS for activating anything with timeout in the controller. This is dummmy.
        
@@ -11,17 +33,5 @@
         return $http.get("").then( function(response) { }, function(error) { });
       };
       */
-     var DB = firebase.database();
-     
-     var getEventList = function() {
-			return DB.ref(constants.REFERENCE01).once(constants.VAL).then(function(snapshot) {
-        return snapshot.val();
-	    });
-     }
-
-      return {
-        getEventList: getEventList
-      }
-
     }]);
 })();
